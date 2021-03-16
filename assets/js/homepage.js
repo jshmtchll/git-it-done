aeb291bcabd918a6bdf22231a6b43094cd55cd8d
josh mitchell <jshmtchll@gmail.com>
2021-03-16 16:03:07 -0500

    added fetch api to make a request to the github api

---
+++ assets/js/homepage.js
@@ -0,0 +1,14 @@
+let getUserRepos = function(user) {
+    //format the github API url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+};
+
+getUserRepos();
